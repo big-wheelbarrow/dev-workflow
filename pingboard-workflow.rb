c623f969ed6d4666959be3bc84295def533acc07
Evan Alter <evan.alter@gmail.com>
2014-07-11 16:24:26 -0500

    Adding more instructions and removing the head

--- pingboard-workflow.rb
+++ pingboard-workflow.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class PingboardWorkflow < Formula
   homepage 'https://github.com/pingboard/git-pivotal-workflow'
-  head 'https://github.com/pingboard/git-pivotal-workflow.git'
+  url 'https://github.com/pingboard/git-pivotal-workflow.git', :using => :git
 
   depends_on 'git-flow-avh'
   depends_on 'pivotal-tracker' => :ruby
@@ -12,7 +12,18 @@ class PingboardWorkflow < Formula
   end
 
   def caveats
-    "You should now system link your .git/hooks directory in your Pingboard code base to ..."
+    s = <<-EOS.undent
+      Add your personal pivotal token and the Pingboard project id to the git config
+
+        git config --add pivotal.token PIVOTAL_TOKEN
+        git config --add pivotal.token PIVOTAL_TOKEN
+        git config --add pivotal.project-id PIVOTAL_PROJECT_ID
+
+
+      Next system link the .git/hooks directory in your Pingboard code base.
+
+        ln -s #{opt_prefix}/hooks CODBASE_PATH/pingboard/.git/hooks
+    EOS
   end
 end
 
